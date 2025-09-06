aee615f3fc3265449cd42c0a9fb796d958644e54
Rodney Woollett <rodney.woollett@gmail.com>
2025-09-06 14:26:37 +1200

    Docker prod

--- src/utility/searchImage.ts
+++ src/utility/searchImage.ts
@@ -3,7 +3,7 @@ import type { ImageData } from "../types/imageData";
 const searchImages = async (term: string): Promise<ImageData[]> => {
   const response = await fetch(`https://api.unsplash.com/search/photos?query=${term}`, {
     headers: {
-      Authorization: `Client-ID ${import.meta.env.VITE_SPLASH_KEY}`
+      //Authorization: `Client-ID ${mport.meta.env.VITE_SPLASH_KEY}`
     },
   });
   const data = await response.json();
