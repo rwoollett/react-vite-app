283dc312a9d3af198cd46d9f775fc719a1a373b8
Rodney Woollett <rodney.woollett@gmail.com>
2026-02-27 14:38:24 +1300

    show mode in running

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,7 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+console.log("Running in mode:", import.meta.env.MODE);
+console.log("Auth server:", import.meta.env.VITE_AUTH_SERVER_URL);
+
