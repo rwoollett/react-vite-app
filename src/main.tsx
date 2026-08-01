e8e482c2272ba25eba89ecd084d228fe3683f171
Rodney Woollett <rodney.woollett@gmail.com>
2026-08-02 10:25:13 +1200

    test banner and schme color

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor, store } from './store/reducers/store'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store/reducers/store';
+import { setColorScheme } from './store/reducers/theme';
 import App from './App.tsx'
+
+// Detect browser/OS dark mode
+const media = window.matchMedia('(prefers-color-scheme: dark)');
+store.dispatch(setColorScheme(media.matches ? 'dark' : 'light'));
+
+media.addEventListener('change', (event) => {
+  store.dispatch(setColorScheme(event.matches ? 'dark' : 'light'));
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
